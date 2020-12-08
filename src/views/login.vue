13fa9b9a3baa1cf1e58c462b58a3abea037941bd
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-12-08 14:45:49 -0400

    created login page

---
+++ src/views/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <form-page-grid title="Login" :onSubmit="onSubmit" :onCancel="onCancel">
+    <app-input v-model="email" placeholder="E-mail" type="email" />
+    <app-input v-model="password" placeholder="Senha" type="password" />
+  </form-page-grid>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AppInput from "../components/app-input.vue";
+import formPageGrid from "../components/form-page-grid.vue";
+
+export default Vue.extend({
+  components: { formPageGrid, AppInput },
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert("submit");
+    },
+    onCancel() {
+      alert("cancel");
+    },
+  },
+});
+</script>
