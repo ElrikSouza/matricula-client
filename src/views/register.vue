017474fe791f60397dc070b3a816a55e97fa06d7
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-12-08 15:40:48 -0400

    Created Register view

---
+++ src/views/register.vue
@@ -0,0 +1,38 @@
+<template>
+  <form-page-grid title="Registro" :onSubmit="onSubmit" :onCancel="onCancel">
+    <app-input v-model="email" placeholder="E-mail" type="email" />
+    <app-input v-model="name" placeholder="Nome" type="email" />
+    <app-input v-model="password" placeholder="Senha" type="password" />
+    <app-input
+      v-model="confirmPassword"
+      placeholder="Confirmar Senha"
+      type="password"
+    />
+  </form-page-grid>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import AppInput from "../components/app-input.vue";
+import formPageGrid from "../components/form-page-grid.vue";
+
+export default Vue.extend({
+  components: { formPageGrid, AppInput },
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert("submit");
+    },
+    onCancel() {
+      alert("cancel");
+    },
+  },
+});
+</script>
